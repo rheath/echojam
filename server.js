fca0714363606a465a4465871f9fb26036cca743
Robert Heath <robertheath@Roberts-MacBook-Air.local>
2026-02-25 18:46:35 -0500

    Add .htaccess entry for server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const { createServer } = require("http");
+const { parse } = require("url");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = process.env.HOST || "0.0.0.0";
+const port = parseInt(process.env.PORT || "3000", 10);
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    createServer((req, res) => {
+      const parsedUrl = parse(req.url, true);
+      handle(req, res, parsedUrl);
+    }).listen(port, hostname, () => {
+      console.log(`EchoJam server listening on http://${hostname}:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Failed to start Next server", err);
+    process.exit(1);
+  });
