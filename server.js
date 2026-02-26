6e64a26bba893ee89a814db1bf858e4d110c389d
Robert Heath <robertheath@Roberts-MacBook-Air.local>
2026-02-26 09:20:46 -0500

    Load runtime env in Passenger standalone startup

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ process.env.NODE_ENV = "production";
 process.env.HOSTNAME = process.env.HOSTNAME || "0.0.0.0";
 
 try {
+  const { loadEnvConfig } = require("@next/env");
+  loadEnvConfig(__dirname);
+} catch (err) {
+  console.warn("Could not load .env files via @next/env", err);
+}
+
+try {
   require("./.next/standalone/server.js");
 } catch (err) {
   console.error("Failed to start standalone Next server", err);
